d00a5a84cdbcea7d6a23137cc31457a08214070d
dstv1 <stoevd1@gmail.com>
2025-03-06 02:05:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,59 +3,65 @@ provider "tfe" {
  organization = "my-test-hcporg"
 }
 
-
-resource "tfe_project" "test-project-1" {   
-  name = "project-1"                  
+resource "tfe_project" "project-1" {
+  name = "project-1"
 }
-
 
 resource "tfe_workspace" "vcs" {
   name                 = "vcs"
-  queue_all_runs       = false  
+  queue_all_runs       = false
+  project_id    = tfe_project.project-1.id
     vcs_repo {
-    branch             = "main"    
-    identifier         = "dstv1/repository"       
-    oauth_token_id     = var.gh_token 
+    branch             = "main"
+    identifier         = "dstv1/repository"
+    oauth_token_id     = var.gh_token
   }
 }
 
-resource "tfe_workspace" "test-CLI-1" {
-  name                 = "workspace-CLI-1"
-       
+resource "tfe_workspace" "CLI-1" {
+  name                 = "CLI-1"
+project_id    = tfe_project.project-1.id
 }
 
-resource "tfe_workspace" "test-CLI-2" {
-  name                 = "workspace-CLI-2"
-  
+resource "tfe_workspace" "CLI-2" {
+  name                 = "CLI-2"
+project_id    = tfe_project.project-1.id
 }
 
-resource "tfe_workspace" "test-CLI-3" {
-  name                 = "workspace-CLI-3"
-  
-
+resource "tfe_workspace" "CLI-3" {
+  name                 = "CLI-3"
+project_id    = tfe_project.project-1.id
 }
 
-
-
-resource "tfe_variable_set" "var-test1" {
-  name         = "var-test1"
-
+resource "tfe_variable_set" "varset-ENV-TF" {
+  name         = "varset-ENV-TF"
 }
 
-resource "tfe_variable" "var-test1-a" {
-  key             = "key1"
-  value           = "value1"
+resource "tfe_variable" "var-TF" {
+  key             = "TF"
+  value           = "Terraform"
   category        = "terraform"
-  description     = "test info"
-  variable_set_id = tfe_variable_set.var-test1.id
-
+  description     = "TF Hi all CLI workspaces"
+  variable_set_id = tfe_variable_set.varset-ENV-TF.id
 }
 
-resource "tfe_variable" "var-test1-b" {
-  key             = "key2"
-  value           = "value2"
+resource "tfe_variable" "var-ENV" {
+  key             = "DEV"
+  value           = "DEV"
   category        = "env"
-  description     = "env var"
-  variable_set_id = tfe_variable_set.var-test1.id
+  description     = "ENV Hi all CLI workspaces"
+  variable_set_id = tfe_variable_set.varset-ENV-TF.id
+}
 
+resource "tfe_workspace_variable_set" "varset-workspaces-ENV-TF-1" {
+  variable_set_id = tfe_variable_set.varset-ENV-TF.id
+  workspace_id    = tfe_workspace.CLI-1.id
+}
+resource "tfe_workspace_variable_set" "varset-workspaces-ENV-TF-2" {
+  variable_set_id = tfe_variable_set.varset-ENV-TF.id
+  workspace_id    = tfe_workspace.CLI-2.id
+}
+resource "tfe_workspace_variable_set" "varset-workspaces-ENV-TF-3" {
+  variable_set_id = tfe_variable_set.varset-ENV-TF.id
+  workspace_id    = tfe_workspace.CLI-3.id
 }
