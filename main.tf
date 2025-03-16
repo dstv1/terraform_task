b624a174c62f7b9c9d523588d330bfc32da18756
dstv1 <stoevd1@gmail.com>
2025-03-16 18:41:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "tfe_oauth_client" "vcs-provider" {
   name             = "my-github-oauth-client"
   organization     = "my-test-hcporg"
   api_url          = "https://api.github.com"
-  http_url         = "https://github.com"
+  http_url         = "https://github.com/dstv1/repository"
   oauth_token      = var.gh_token
   service_provider = "github"
   organization_scoped = true
