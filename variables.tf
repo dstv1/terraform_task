2396f1a7ffeaafb6ce66e7bd7e53c2289e9b37aa
dstv1 <stoevd1@gmail.com>
2025-09-08 18:38:20 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,24 @@
 variable "organization" {
   description = "my-test-hcporg"
   type        = string
-  sensitive   = true
 }
 
 variable "terraform_api_token" {
   description = "The token used to authenticate with Terraform Cloud/Enterprise"
   type        = string
-  sensitive   = true
 }
 
 variable "github_api_token" {
   description = "The token used to authenticate with Github"
   type        = string
-  sensitive   = true
 }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description  = "allowing your workspaces to authenticate to AWS"
+variable "AWS_REGION" {
+  description  = "AWS region"
   type        = string
 }
 
 variable "region" {
-  description  = "The AWS region to deploy resources into"
+  description = "deployment region"
   type        = string
 }
